fcbd5ce153aa9cf74f77dd1cdab0d8baa9e71cab
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-17 12:06:49 -0600

    Feat: establish validations and relationships for Merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,9 @@
 class Merchant < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices 
 end
