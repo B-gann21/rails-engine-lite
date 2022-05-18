050ae285d668a848069c6e4f7421ed1b68baf0dd
Brylan Gannon <brylan.gannon112@gmail.com>
2022-05-17 20:00:46 -0600

    Feat: Item Show endpoint returns customized error object when ID is invalid

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ItemsController < ApplicationController
   before_action :get_item, only: [:show]
+  rescue_from ::ActiveRecord::RecordNotFound, with: :item_not_found
   
   def index
     render json: ItemSerializer.items_index(Item.all)
@@ -27,5 +28,9 @@ class Api::V1::ItemsController < ApplicationController
 
   def item_params
     params.permit(:name, :description, :unit_price, :merchant_id)
+  end
+
+  def item_not_found
+    render json: ItemErrorSerializer.no_item(params[:id]), status: 404
   end
 end
