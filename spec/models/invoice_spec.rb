835d51f312af2b1f9ce9e6679b0ff98789c20928
Brylan Gannon <brylan.gannon112@gmail.com>
2022-10-07 08:47:48 -0600

    Update invoice_spec.rb

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Invoice do
     it { should have_many(:items).through(:invoice_items) } 
   end
   
-  it 'is a test to check how gh actions works' do
+  xit 'is a test to check how gh actions works' do
     expect(1+1).to eq 2
   end
 end
